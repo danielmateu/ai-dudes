63e44f41897c935b828ea2af99f11ef94871afcc
Daniel Mateu <danielmateu86@gmail.com>
2023-08-23 11:39:26 +0200

    favicon changed

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,8 +9,11 @@ import { ProModal } from '@/components/ProModal'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'AI Dudes',
+  title: 'Dudes AI',
   description: 'Habla con las mentes más potentes del mundo',
+  keywords: 'AI, Inteligencia Artificial, Chatbot, Chat, Bots, Bot',
+  viewport: 'width=device-width, initial-scale=1',
+  creator: 'Daniel Mateu',
 }
 
 export default function RootLayout({
@@ -21,6 +24,9 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="es" suppressHydrationWarning>
+        <head>
+          <link rel="icon" href="/favicon.ico" sizes="any" />
+        </head>
         <body className={cn("bg-secondary ", inter.className)}>
           <ThemeProvider attribute='class' defaultTheme='system' enableSystem>
             <ProModal />
